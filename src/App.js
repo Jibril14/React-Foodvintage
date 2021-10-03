82728331661f10d09520add625b52b4a9a8a77fa
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2021-10-03 06:42:03 +0100

    Route More Components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 import Layout from "./Components/Layout/Layout";
-import Restaurant from "./Containers/Restaurant/Restaurant";
 import Checkout from "./Components/Checkout/Checkout";
 import Order from "./Components/Order/Order";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import Grid from "./Components/Grid/Grid";
+import Joinus from "./Components/JoinUs/Joinus";
 
 class App extends Component {
     render() {
         return (
             <Layout>
-                <Route path="/order" component={Order} />
-                <Route path="/checkout" component={Checkout} />
-                <Route path="/" exact component={Restaurant} />
+                <Switch>
+                    <Route path="/order" component={Order} />
+                    <Route path="/checkout" component={Checkout} />
+                    <Route path="/join-us" component={Joinus} />
+                    <Route path="/" component={Grid} />
+                </Switch>
             </Layout>
         );
     }
